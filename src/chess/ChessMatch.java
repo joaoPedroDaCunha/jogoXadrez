9e1007f9d7350c6aeefe7eaf3e1168eb4b4778dc
joaoPedroDaCunhaSantos <joaopedrodacunha.santos@gmail.com>
2023-06-29 15:45:45 -0300

    Handling captured pieces

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,5 +1,8 @@
 package chess;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import boardgame.Board;
 import boardgame.Piece;
 import boardgame.Position;
@@ -10,6 +13,9 @@ public class ChessMatch {
 	private int turn;
 	private Color currentPlayer;
 	private Board board;
+	
+	private List<Piece> piecesOnTheBoard = new ArrayList<>();
+	private List<Piece> capturedPieces = new ArrayList<>();
 	
 	public ChessMatch() {
 		board = new Board(8, 8);
@@ -57,7 +63,7 @@ public class ChessMatch {
 			throw new ChessException("There is no piece no source position");
 		}
 		if(currentPlayer !=((ChessPiece) board.piece(position)).getColor()){
-			throw new ChessException("thr chose piece is bot yours");
+			throw new ChessException("the chose piece is bot yours");
 		}
 		if(!board.piece(position).isThereAnyPossibleMove()) {
 			throw new ChessException("There is no possible moves for the chose piece");
@@ -79,11 +85,18 @@ public class ChessMatch {
 		Piece p = board.removePiece(source);
 		Piece capturePiece = board.removePiece(target);
 		board.placePiece(p, target);
+		
+		if(capturePiece != null) {
+			piecesOnTheBoard.remove(capturePiece);
+			capturedPieces.add(capturePiece);
+		}
+		
 		return capturePiece;
 	}
 	
 	private void placeNewPiece(char column,int row, ChessPiece piece) {
 		board.placePiece(piece, new ChessPosition(column,row).toposition());
+	    piecesOnTheBoard.add(piece);
 	}
 	
 	private void initialSetup() {
