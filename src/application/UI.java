9b1ca9b96eef0b984dfa602bed98b16b3fa8bb76
joaoPedroDaCunhaSantos <joaopedrodacunha.santos@gmail.com>
2023-06-30 16:31:36 -0300

    Checkmate logic

--- src/application/UI.java
+++ src/application/UI.java
@@ -58,9 +58,14 @@ public class UI {
 		printCapturedPiece(captured);
 		System.out.println();
 		System.out.println("Turn : " + chessMatch.getTurn());
-		System.out.println("Witting palyer: " + chessMatch.getCurrentPlater());
-		if(chessMatch.getCheck()) {
-			System.out.println("Check");
+		if(!chessMatch.getCheckMate()) {
+			System.out.println("Witting palyer: " + chessMatch.getCurrentPlayer());
+			if(chessMatch.getCheck()) {
+				System.out.println("Check");
+			}
+		}else {
+			System.out.println("CHECKMATE");
+			System.out.println("Winner "+chessMatch.getCurrentPlayer());
 		}
 	}
 	
